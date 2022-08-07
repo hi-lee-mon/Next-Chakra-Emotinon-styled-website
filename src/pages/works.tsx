20f18bb42bed68fa2d293d1bd61d3e47574d0e55
shun <shunmel.09@gmail.com>
2022-08-07 13:59:32 +0900

    worksページの表示を微修正(delayを0sに修正)

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -11,13 +11,13 @@ const Works = () => {
         Works
       </Heading>
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay='0.1s'>
+        <Section>
           <WorkGridItem id='inkdrop' title='Inkdrop' thumbnail={thumbInkdrop}>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, error alias. Exercitationem, nihil nesciunt ipsum similique id quisquam
             ad autem nisi quasi quas, obcaecati porro quam explicabo minima sunt accusamus!
           </WorkGridItem>
         </Section>
-        <Section delay='0.1s'>
+        <Section>
           <WorkGridItem id='walknote' title='Walknote' thumbnail={thumbWalknote}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos assumenda sequi earum itaque, unde iste debitis blanditiis architecto, sunt
             animi quisquam soluta, eius eaque aliquid explicabo ipsa rerum velit voluptates.
